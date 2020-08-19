f2a4f7013f88eeb93cd6758eaba7d56dc11f4477
Yasir <yaser_nami@hotmail.com>
2020-08-19 06:15:19 +0300

    heroku test

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS, cross_origin
 
-from models import setup_db, Movies, Actors
-from auth import AuthError, requires_auth
+from backend.models import setup_db, Movies, Actors
+from backend.auth import AuthError, requires_auth
 # heroku scale worker=1
 app = Flask(__name__)
 setup_db(app)
@@ -36,7 +36,7 @@ def after_request(response):
 @app.route('/')
 @cross_origin()
 def index():
-    return render_template('../frontend/index.html', title='Home')
+    return render_template('index.html', title='Home')
 
 
 # db_drop_and_create_all()
