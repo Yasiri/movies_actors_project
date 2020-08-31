05893beb9f75bbd32186fa1557780855f5941a9b
Yasir <yaser_nami@hotmail.com>
2020-08-31 16:25:38 +0300

    test

--- api.py
+++ api.py
@@ -283,7 +283,7 @@ def createMovie(payload):
 
         datetime_obj = datetime.datetime.strptime(
             movie_release_date, '%Y-%m-%d')
-
+        print('date ob ', datetime_obj)
         new_movie = Movies(
             title=movie_title,
             release_date=datetime_obj,
